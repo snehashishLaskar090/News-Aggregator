cdcee9237a95d212a865f496cb6357b458f83bcf
Suman Laskar <sumanlaskar@Sumans-MacBook-Air.local>
2022-10-13 17:41:33 +0530

    initial setup

--- scraper.py
+++ scraper.py
@@ -1,3 +1,13 @@
 # Author: Snehashish Laskar
 # Date: 13-10-2022
 
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.hindustantimes.com/latest-news"
+html = BeautifulSoup(requests.get(url).text, "html.parser")
+var = html.find_all("a")
+print(requests.get(url).text)
+
+
+
